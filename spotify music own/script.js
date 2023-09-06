70ae1c17906e89c23aecc2d7173a95e447da7b5e
srinadh7 <121044889+srinadhkesineni@users.noreply.github.com>
2023-09-06 13:51:42 +0530

    Add files via upload

---
+++ spotify music own/script.js
@@ -0,0 +1,107 @@
+console.log('welcome to spotify');
+
+let songIndex=0;
+let audioElement=new Audio('songs/1.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+
+let songs=[
+    {songName:"Tiger ka hukum", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"Master coming", filePath:"songs/2.mp3", coverPath:"covers/2.jpg"},
+    {songName:"Mass maranam", filePath:"songs/3.mp3", coverPath:"covers/3.jpg"},
+    {songName:"Gaali valuga", filePath:"songs/4.mp3", coverPath:"covers/4.jpg"},
+    {songName:"Vikram Title song", filePath:"songs/5.mp3", coverPath:"covers/5.jpg"},
+    {songName:"The Karma Theme", filePath:"songs/6.mp3", coverPath:"covers/6.jpg"},
+    {songName:"Why This kolavari Di?", filePath:"songs/7.mp3", coverPath:"covers/7.jpg"},
+]
+
+songItems.forEach((Element,i)=>{
+    Element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    Element.getElementsByClassName('songName')[0].innerText=songs[i].songName;
+})
+
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.add('fa-play-circle');
+        masterPlay.classList.remove('fa-pause-circle');
+        gif.style.opacity=0;
+    }
+})
+
+audioElement.addEventListener('timeupdate',()=>{
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value=progress;
+})
+
+myProgressBar.addEventListener('change',()=>{
+     audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((Element)=>{
+    Element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+
+
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=7){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
+
+
+
